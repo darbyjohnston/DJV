96518d9b92ee8e41786b8323d97ea02e4ad069aa
Darby Johnston <darbyjohnston@yahoo.com>
2015-05-09 17:17:31 -0700

    Update paths

--- etc/Windows/vs2013-cmd-32.bat
+++ etc/Windows/vs2013-cmd-32.bat
@@ -1,8 +1,8 @@
-set CMAKE_PREFIX_PATH=C:/Users/darby/Projects/djv/djv-git-third-party-Windows-32
+set CMAKE_PREFIX_PATH=C:/Projects/djv/djv-git-third-party-Windows-32
 
-set PATH=%PATH%;C:\Users\darby\Projects\djv\djv-git-third-party-Windows-32\bin
+set PATH=%PATH%;C:\Projects\djv\djv-git-third-party-Windows-32\bin
 set PATH=%PATH%;C:\Program Files (x86)\NSIS
 
-cd C:\Users\darby\Projects\djv
+cd C:\Projects\djv
 
 %comspec% /k "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86
--- etc/Windows/vs2013-cmd-64.bat
+++ etc/Windows/vs2013-cmd-64.bat
@@ -1,8 +1,8 @@
-set CMAKE_PREFIX_PATH=C:/Users/darby/Projects/djv/djv-git-third-party-Windows-64
+set CMAKE_PREFIX_PATH=C:/Projects/djv/djv-git-third-party-Windows-64
 
-set PATH=%PATH%;C:\Users\darby\Projects\djv\djv-git-third-party-Windows-64\bin
+set PATH=%PATH%;C:\Projects\djv\djv-git-third-party-Windows-64\bin
 set PATH=%PATH%;C:\Program Files (x86)\NSIS
 
-cd C:\Users\darby\Projects\djv
+cd C:\Projects\djv
 
 %comspec% /k "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x64
