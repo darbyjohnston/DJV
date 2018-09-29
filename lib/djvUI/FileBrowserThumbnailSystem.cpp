509f60ef2ec59364b07bcd89c447ae54399dc7eb
Darby Johnston <darbyjohnston@yahoo.com>
2018-09-29 12:27:45 -0700

    Remove unused code

--- lib/djvUI/FileBrowserThumbnailSystem.cpp
+++ lib/djvUI/FileBrowserThumbnailSystem.cpp
@@ -114,20 +114,6 @@ namespace djv
                 std::promise<QPixmap> promise;
             };
 
-            void MessageCallback(
-                GLenum source,
-                GLenum type,
-                GLuint id,
-                GLenum severity,
-                GLsizei length,
-                const GLchar* message,
-                const void* userParam)
-            {
-                fprintf(stderr, "GL CALLBACK: %s type = 0x%x, severity = 0x%x, message = %s\n",
-                    (type == GL_DEBUG_TYPE_ERROR ? "** GL ERROR **" : ""),
-                    type, severity, message);
-            }
-
         } // namespace
 
         struct FileBrowserThumbnailSystem::Private
