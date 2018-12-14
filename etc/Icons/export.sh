3b04c95f34eaf218bbcceff2fe662d32c338cd07
Darby Johnston <darbyjohnston@yahoo.com>
2018-12-14 14:49:47 -0800

    Add icons

---
+++ etc/Icons/export.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+dpiList="90 180"
+
+for svg in *.svg
+do
+    for dpi in ${dpiList}
+    do
+        python ../Util/svg-objects-export-master/svg-objects-export.py -f -P "FILE" -PF "${dpi}DPI" --extra "--export-dpi $dpi" $svg
+    done
+done
+
