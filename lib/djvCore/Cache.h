dba97e2fcfeda7585d67d755de03a22b06ed4da2
Darby Johnston <darbyjohnston@yahoo.com>
2019-08-07 15:03:22 -0700

    Fix typo

--- lib/djvCore/Cache.h
+++ lib/djvCore/Cache.h
@@ -42,7 +42,7 @@ namespace djv
         {
             //! This class provides a cache.
             //!
-            //! \todo Return an interator from get() instead of a value.
+            //! \todo Return an iterator from get() instead of a value.
             template<typename T, typename U>
             class Cache
             {
