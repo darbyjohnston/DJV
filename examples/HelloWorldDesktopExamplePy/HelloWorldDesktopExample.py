26dc92db2260c564bbaec8083b438ddc65eb009a
Darby Johnston <darbyjohnston@yahoo.com>
2019-08-18 14:00:18 -0700

    Indentation fix

--- examples/HelloWorldDesktopExamplePy/HelloWorldDesktopExample.py
+++ examples/HelloWorldDesktopExamplePy/HelloWorldDesktopExample.py
@@ -5,14 +5,14 @@ import sys
 
 try:
 	app = djvDesktopAppPy.Application.create(sys.argv)
-
-    label = djvUIPy.Label.create(app)
-    label.setText("Hello world!")
-
+    
+	label = djvUIPy.Label.create(app)
+	label.setText("Hello world!")
+    
 	window = djvUIPy.Window.create(app)
 	window.addChild(label)
 	window.show()
-
+    
 	app.run()
 
 except (error):
