69ac0c0ae37f8178f4cc641132f1eaa050faaf9b
Darby Johnston <darbyjohnston@yahoo.com>
2019-11-16 18:31:21 -0800

    https://github.com/darbyjohnston/DJV/issues/159

--- cmake/Modules/BuildFFmpeg.cmake
+++ cmake/Modules/BuildFFmpeg.cmake
@@ -1,35 +1,39 @@
 include(ExternalProject)
 
 if(NOT DJV_THIRD_PARTY_DISABLE_BUILD)
-    set(FFmpeg_EXTRA_CFLAGS)
-    set(FFmpeg_EXTRA_CXXFLAGS)
-    set(FFmpeg_EXTRA_LDFLAGS)
-    set(FFmpeg_CONFIGURE_ARGS
-        --prefix=${CMAKE_INSTALL_PREFIX}
-        --disable-programs
-        --disable-bzlib
-        --disable-iconv
-        --disable-lzma
-        --disable-appkit
-        --disable-avfoundation
-        --disable-coreimage
-        --disable-audiotoolbox
-        --enable-pic
-        --extra-cflags=${FFmpeg_EXTRA_CFLAGS}
-        --extra-cxxflags=${FFmpeg_EXTRA_CXXFLAGS}
-        --extra-ldflags=${FFmpeg_EXTRA_LDFLAGS})
-    if(FFmpeg_SHARED_LIBS)
-        set(FFmpeg_CONFIGURE_ARGS ${FFmpeg_CONFIGURE_ARGS} --disable-static --enable-shared)
+    if(WIN32)
+        # See the directions for building FFmpeg on Windows in "docs/build_windows.html".
+    else()
+        set(FFmpeg_EXTRA_CFLAGS)
+        set(FFmpeg_EXTRA_CXXFLAGS)
+        set(FFmpeg_EXTRA_LDFLAGS)
+        set(FFmpeg_CONFIGURE_ARGS
+            --prefix=${CMAKE_INSTALL_PREFIX}
+            --disable-programs
+            --disable-bzlib
+            --disable-iconv
+            --disable-lzma
+            --disable-appkit
+            --disable-avfoundation
+            --disable-coreimage
+            --disable-audiotoolbox
+            --enable-pic
+            --extra-cflags=${FFmpeg_EXTRA_CFLAGS}
+            --extra-cxxflags=${FFmpeg_EXTRA_CXXFLAGS}
+            --extra-ldflags=${FFmpeg_EXTRA_LDFLAGS})
+        if(FFmpeg_SHARED_LIBS)
+            set(FFmpeg_CONFIGURE_ARGS ${FFmpeg_CONFIGURE_ARGS} --disable-static --enable-shared)
+        endif()
+        ExternalProject_Add(
+            FFmpegThirdParty
+            PREFIX ${CMAKE_CURRENT_BINARY_DIR}/FFmpeg
+            DEPENDS ZLIBThirdParty
+            URL http://ffmpeg.org/releases/ffmpeg-4.2.tar.bz2
+            BUILD_IN_SOURCE 1
+            CONFIGURE_COMMAND ./configure ${FFmpeg_CONFIGURE_ARGS}
+            BUILD_COMMAND $(MAKE)
+            INSTALL_COMMAND $(MAKE) install)
     endif()
-    ExternalProject_Add(
-        FFmpegThirdParty
-        PREFIX ${CMAKE_CURRENT_BINARY_DIR}/FFmpeg
-        DEPENDS ZLIBThirdParty
-        URL http://ffmpeg.org/releases/ffmpeg-4.2.tar.bz2
-        BUILD_IN_SOURCE 1
-        CONFIGURE_COMMAND ./configure ${FFmpeg_CONFIGURE_ARGS}
-        BUILD_COMMAND $(MAKE)
-        INSTALL_COMMAND $(MAKE) install)
 endif()
 
 set(FFmpeg_FOUND TRUE)
