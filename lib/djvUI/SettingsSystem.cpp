9e3d0e6cbb9091aa59e7c0c4a5c80fc81ed64e73
Darby Johnston <darbyjohnston@yahoo.com>
2019-11-16 19:08:41 -0800

    Bump version number

--- lib/djvUI/SettingsSystem.cpp
+++ lib/djvUI/SettingsSystem.cpp
@@ -49,7 +49,7 @@ namespace djv
         {
             namespace
             {
-                const size_t settingsVersion = 11;
+                const size_t settingsVersion = 12;
 
             } // namespace
 
