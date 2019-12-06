ad556bfe5c0f58a2672c7ff9c5b4c2e74800312f
Darby Johnston <darbyjohnston@yahoo.com>
2019-12-06 10:18:39 -0800

    Use an array instead of a vector

--- lib/djvCore/BBoxInline.h
+++ lib/djvCore/BBoxInline.h
@@ -509,22 +509,22 @@ namespace djv
     template<typename T, glm::precision P>
     inline Core::BBox::tBBox3<T, P> operator * (const Core::BBox::tBBox3<T, P> & bbox, const glm::mat4 & m)
     {
-        std::vector<glm::tvec4<T, P> > pts;
-        pts.push_back(glm::tvec4<T, P>(bbox.min.x, bbox.min.y, bbox.min.z, 1.F));
-        pts.push_back(glm::tvec4<T, P>(bbox.max.x, bbox.min.y, bbox.min.z, 1.F));
-        pts.push_back(glm::tvec4<T, P>(bbox.max.x, bbox.min.y, bbox.max.z, 1.F));
-        pts.push_back(glm::tvec4<T, P>(bbox.min.x, bbox.min.y, bbox.max.z, 1.F));
-        pts.push_back(glm::tvec4<T, P>(bbox.min.x, bbox.max.y, bbox.min.z, 1.F));
-        pts.push_back(glm::tvec4<T, P>(bbox.max.x, bbox.max.y, bbox.min.z, 1.F));
-        pts.push_back(glm::tvec4<T, P>(bbox.max.x, bbox.max.y, bbox.max.z, 1.F));
-        pts.push_back(glm::tvec4<T, P>(bbox.min.x, bbox.max.y, bbox.max.z, 1.F));
-
+        const glm::tvec4<T, P> pts[] =
+        {
+            glm::tvec4<T, P>(bbox.min.x, bbox.min.y, bbox.min.z, 1.F),
+            glm::tvec4<T, P>(bbox.max.x, bbox.min.y, bbox.min.z, 1.F),
+            glm::tvec4<T, P>(bbox.max.x, bbox.min.y, bbox.max.z, 1.F),
+            glm::tvec4<T, P>(bbox.min.x, bbox.min.y, bbox.max.z, 1.F),
+            glm::tvec4<T, P>(bbox.min.x, bbox.max.y, bbox.min.z, 1.F),
+            glm::tvec4<T, P>(bbox.max.x, bbox.max.y, bbox.min.z, 1.F),
+            glm::tvec4<T, P>(bbox.max.x, bbox.max.y, bbox.max.z, 1.F),
+            glm::tvec4<T, P>(bbox.min.x, bbox.max.y, bbox.max.z, 1.F)
+        };
         Core::BBox::tBBox3<T, P> out(m * pts[0]);
         for (size_t i = 1; i < 8; ++i)
         {
             out.expand(m * pts[i]);
         }
-
         return out;
     }
 
