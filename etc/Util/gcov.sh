1e4f8a784bd897ff55d9c2ec8965bf45fc6433cc
Darby Johnston <darbyjohnston@yahoo.com>
2019-12-20 15:35:19 -0800

    Troubleshooting Travis CI build failure

--- etc/Util/gcov.sh
+++ etc/Util/gcov.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
-gcnoList=`find $PWD -name "*.gcno" -and \( -not -name "CMakeC*CompilerId.gcno" \)`
-echo "gcnoList="$gcnoList
-
+#gcnoList=`find $PWD -name "*.gcno" -and \( -not -name "CMakeC*CompilerId.gcno" \)`
+#echo "gcnoList="$gcnoList
 for gcno in $gcnoList
 do
-    #echo $gcno
+    echo "gcno="$gcno
     srcName=`basename -s .gcno $gcno`
-    #echo $srcName
+    echo "srcName="$srcName
     src=`find $1 -name $srcName`
-    #echo $src
+    echo "src"$src
     gcov -o $gcno $src
 done
