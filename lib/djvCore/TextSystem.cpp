0fd79a814b6d0b55434052d71e66ad29afffd26b
Darby Johnston <darbyjohnston@yahoo.com>
2019-12-21 16:00:15 -0800

    Make the file stats async

--- lib/djvCore/TextSystem.cpp
+++ lib/djvCore/TextSystem.cpp
@@ -64,6 +64,7 @@ namespace djv
             std::shared_ptr<ValueSubject<bool> > textChanged;
 
             std::vector<std::future<TextMap> > readFutures;
+            std::future<std::vector<FileSystem::FileInfo> > statFuture;
             std::shared_ptr<Time::Timer> timer;
         };
 
@@ -268,14 +269,43 @@ namespace djv
                             system->_p->textChanged->setAlways(true);
                         }
 
-                        for (auto j = system->_p->textFiles.begin(); j != system->_p->textFiles.end(); ++j)
+                        bool stat = false;
+                        if (system->_p->statFuture.valid())
                         {
-                            FileSystem::FileInfo fileInfo(j->getFileName());
-                            if (fileInfo.getTime() > j->getTime())
+                            if (system->_p->statFuture.wait_for(std::chrono::seconds(0)) == std::future_status::ready)
                             {
-                                system->_reload(fileInfo);
-                                *j = fileInfo;
+                                auto textFiles = system->_p->statFuture.get();
+                                for (auto j = system->_p->textFiles.begin(), k = textFiles.begin();
+                                    j != system->_p->textFiles.end(), k != textFiles.end();
+                                    ++j, ++k)
+                                {
+                                    if (k->getTime() > j->getTime())
+                                    {
+                                        system->_reload(*k);
+                                        *j = *k;
+                                    }
+                                }
+                                stat = true;
                             }
+                        }
+                        else
+                        {
+                            stat = true;
+                        }
+                        if (stat)
+                        {
+                            auto textFiles = system->_p->textFiles;
+                            system->_p->statFuture = std::async(
+                                std::launch::async,
+                                [textFiles]
+                                {
+                                    std::vector<FileSystem::FileInfo> out;
+                                    for (const auto& i : textFiles)
+                                    {
+                                        out.push_back(FileSystem::FileInfo(i.getPath()));
+                                    }
+                                    return out;
+                                });
                         }
                     }
                 });
