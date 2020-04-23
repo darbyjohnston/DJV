019cd65d12b11b7ab506c321cad94fb4151f2439
Darby Johnston <darbyjohnston@yahoo.com>
2020-04-22 18:35:57 -0700

    Add debugging

--- etc/Util/gcov.sh
+++ etc/Util/gcov.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
 dirList=`cd $1 && find lib tests -mindepth 1 -maxdepth 1 -type d`
-#echo "dirList="$dirList
+echo "dirList="$dirList
 for dir in $dirList
 do
     gcnoList=`find $dir -name "*.gcno" -and \( -not -name "CMakeC*CompilerId.gcno" \)`
-    #echo "gcnoList="$gcnoList
+    echo "gcnoList="$gcnoList
     for gcno in $gcnoList
     do
-        #echo "gcno="$gcno
+        echo "gcno="$gcno
         srcName=`basename -s .gcno $gcno`
-        #echo "srcName="$srcName
+        echo "srcName="$srcName
         src=`find $1/$dir -name $srcName`
-        #echo "src="$src
+        echo "src="$src
         gcov -o $gcno $src
     done
 done
