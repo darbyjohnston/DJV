aa2f2ba7dc6d1471ae1574c974a9722c63cd0211
Darby Johnston <darbyjohnston@yahoo.com>
2020-05-16 11:28:22 -0700

    Replace tabs

--- etc/Shaders/GL4_1/djvAVRender2DFragment.glsl
+++ etc/Shaders/GL4_1/djvAVRender2DFragment.glsl
@@ -137,7 +137,7 @@ void main()
     }
     else if (COLOR_MODE_COLOR_WITH_TEXTURE_ALPHA == colorMode)
     {
-		vec4 t = texture(textureSampler, Texture);
+        vec4 t = texture(textureSampler, Texture);
         FragColor.r = color.r;
         FragColor.g = color.g;
         FragColor.b = color.b;
@@ -145,7 +145,7 @@ void main()
     }
     else if (COLOR_MODE_COLOR_WITH_TEXTURE_ALPHA_R == colorMode)
     {
-		vec4 t = texture(textureSampler, Texture);
+        vec4 t = texture(textureSampler, Texture);
         FragColor.r = color.r;
         FragColor.g = 0.0;
         FragColor.b = 0.0;
@@ -153,7 +153,7 @@ void main()
     }
     else if (COLOR_MODE_COLOR_WITH_TEXTURE_ALPHA_G == colorMode)
     {
-		vec4 t = texture(textureSampler, Texture);
+        vec4 t = texture(textureSampler, Texture);
         FragColor.r = 0.0;
         FragColor.g = color.g;
         FragColor.b = 0.0;
@@ -161,7 +161,7 @@ void main()
     }
     else if (COLOR_MODE_COLOR_WITH_TEXTURE_ALPHA_B == colorMode)
     {
-		vec4 t = texture(textureSampler, Texture);
+        vec4 t = texture(textureSampler, Texture);
         FragColor.r = 0.0;
         FragColor.g = 0.0;
         FragColor.b = color.b;
@@ -169,25 +169,25 @@ void main()
     }
     else if (COLOR_MODE_COLOR_AND_TEXTURE == colorMode)
     {
-		// Sample the texture.
-		vec4 t = texture(textureSampler, Texture);
-		
-		// Swizzle the channels for the given image format.
-		if (IMAGE_CHANNELS_L == imageChannels)
-		{
-			t.g = t.b = t.r;
-			t.a = 1.0;
-		}
-		else if (IMAGE_CHANNELS_LA == imageChannels)
-		{
-			t.a = t.g;
-			t.g = t.b = t.r;
-		}
-		else if (IMAGE_CHANNELS_RGB == imageChannels)
-		{
-			t.a = 1.0;
-		}
-		
+        // Sample the texture.
+        vec4 t = texture(textureSampler, Texture);
+
+        // Swizzle the channels for the given image format.
+        if (IMAGE_CHANNELS_L == imageChannels)
+        {
+            t.g = t.b = t.r;
+            t.a = 1.0;
+        }
+        else if (IMAGE_CHANNELS_LA == imageChannels)
+        {
+            t.a = t.g;
+            t.g = t.b = t.r;
+        }
+        else if (IMAGE_CHANNELS_RGB == imageChannels)
+        {
+            t.a = 1.0;
+        }
+        
 //$colorSpaceBody
 
         if (colorMatrixEnabled)
@@ -219,27 +219,27 @@ void main()
             t.g = t.r;
             t.b = t.r;
         }
-		else if (IMAGE_CHANNEL_GREEN == imageChannel)
-		{
-			t.r = t.g;
-			t.b = t.g;
-		}
-		else if (IMAGE_CHANNEL_BLUE == imageChannel)
-		{
-			t.r = t.b;
-			t.g = t.b;
-		}
-		else if (IMAGE_CHANNEL_ALPHA == imageChannel)
-		{
-			t.r = t.a;
-			t.g = t.a;
-			t.b = t.a;
-		}
-		
+        else if (IMAGE_CHANNEL_GREEN == imageChannel)
+        {
+            t.r = t.g;
+            t.b = t.g;
+        }
+        else if (IMAGE_CHANNEL_BLUE == imageChannel)
+        {
+            t.r = t.b;
+            t.g = t.b;
+        }
+        else if (IMAGE_CHANNEL_ALPHA == imageChannel)
+        {
+            t.r = t.a;
+            t.g = t.a;
+            t.b = t.a;
+        }
+
         FragColor = t * color;
     }
-	else if (COLOR_MODE_SHADOW == colorMode)
-	{
+    else if (COLOR_MODE_SHADOW == colorMode)
+    {
         FragColor = color * Texture.x;
-	}
+    }
 }
