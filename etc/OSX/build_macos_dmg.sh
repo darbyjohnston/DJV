0ac395d5db47690238500999529923ab757f1403
Darby Johnston <darbyjohnston@yahoo.com>
2020-06-10 11:01:53 -0700

    Parallel builds

--- etc/OSX/build_macos_dmg.sh
+++ etc/OSX/build_macos_dmg.sh
@@ -9,15 +9,15 @@ export DYLD_LIBRARY_PATH=$DJV_BUILD/DJV-install-Release/lib:$DYLD_LIBRARY_PATH
 mkdir -p DJV-third-party-Release
 cd DJV-third-party-Release
 cmake ../DJV/third-party -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$DJV_BUILD/DJV-install-Release -DCMAKE_FIND_FRAMEWORK="LAST"
-cmake --build .
-cmake --build . --target install
+cmake --build . -j 16
+cmake --build . -j 16 --target install
 cd ..
 
 mkdir -p DJV-Release
 cd DJV-Release
 cmake ../DJV -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$DJV_BUILD/DJV-install-Release -DCMAKE_PREFIX_PATH=$DJV_BUILD/DJV-install-Release -DCMAKE_FIND_FRAMEWORK="LAST"
-cmake --build .
+cmake --build . -j 16
 
 cmake ../DJV -DDJV_THIRD_PARTY=TRUE
-cmake --build .
+cmake --build . -j 16
 cmake --build . --target package
