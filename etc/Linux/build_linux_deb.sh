96bdea5d673c28228b7978898b36a806a730cfda
Darby Johnston <darbyjohnston@yahoo.com>
2020-06-10 09:55:59 -0700

    Script fixes

--- etc/Linux/build_linux_deb.sh
+++ etc/Linux/build_linux_deb.sh
@@ -6,14 +6,14 @@ export DJV_BUILD=$PWD
 export PATH=$DJV_BUILD/DJV-install-Release/bin:$PATH
 export LD_LIBRARY_PATH=$DJV_BUILD/DJV-install-Release/lib:$LD_LIBRARY_PATH
 
-mkdir DJV-third-party-Release
+mkdir -p DJV-third-party-Release
 cd DJV-third-party-Release
 cmake ../DJV/third-party -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$DJV_BUILD/DJV-install-Release
 cmake --build . -j 16
 cmake --build . -j 16 --target install
 cd ..
 
-mkdir DJV-Release
+mkdir -p DJV-Release
 cd DJV-Release
 cmake ../DJV -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$DJV_BUILD/DJV-install-Release -DCMAKE_PREFIX_PATH=$DJV_BUILD/DJV-install-Release
 cmake --build . -j 16
--- etc/OSX/build_macos_dmg.sh
+++ etc/OSX/build_macos_dmg.sh
@@ -6,14 +6,14 @@ export DJV_BUILD=$PWD
 export PATH=$DJV_BUILD/DJV-install-Release/bin:$PATH
 export DYLD_LIBRARY_PATH=$DJV_BUILD/DJV-install-Release/lib:$DYLD_LIBRARY_PATH
 
-mkdir DJV-third-party-Release
+mkdir -p DJV-third-party-Release
 cd DJV-third-party-Release
 cmake ../DJV/third-party -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$DJV_BUILD/DJV-install-Release -DCMAKE_FIND_FRAMEWORK="LAST"
 cmake --build .
 cmake --build . --target install
 cd ..
 
-mkdir DJV-Release
+mkdir -p DJV-Release
 cd DJV-Release
 cmake ../DJV -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$DJV_BUILD/DJV-install-Release -DCMAKE_PREFIX_PATH=$DJV_BUILD/DJV-install-Release -DCMAKE_FIND_FRAMEWORK="LAST"
 cmake --build .
