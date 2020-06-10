9388e8711b8fd41f4d0c04c5c6c63f3336aff3a5
Darby Johnston <darbyjohnston@yahoo.com>
2020-06-10 08:59:43 -0700

    Add packaging script

---
+++ etc/OSX/build_macos_dmg.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Run this script in the parent directory of the repository.
+
+export DJV_BUILD=$PWD
+export PATH=$DJV_BUILD/DJV-install-Release/bin:$PATH
+export DYLD_LIBRARY_PATH=$DJV_BUILD/DJV-install-Release/lib:$DYLD_LIBRARY_PATH
+
+mkdir DJV-third-party-Release
+cd DJV-third-party-Release
+cmake ../DJV/third-party -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$DJV_BUILD/DJV-install-Release -DCMAKE_FIND_FRAMEWORK="LAST"
+cmake --build .
+cmake --build . --target install
+cd ..
+
+mkdir DJV-Release
+cd DJV-Release
+cmake ../DJV -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$DJV_BUILD/DJV-install-Release -DCMAKE_PREFIX_PATH=$DJV_BUILD/DJV-install-Release -DCMAKE_FIND_FRAMEWORK="LAST"
+cmake --build .
+
+cmake ../DJV -DDJV_THIRD_PARTY=TRUE
+cmake --build .
+cmake --build . --target package
