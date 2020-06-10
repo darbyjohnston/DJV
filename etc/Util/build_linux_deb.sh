e538e6f30cde3f2c2a58ba41168c102d383efef8
Darby Johnston <darbyjohnston@yahoo.com>
2020-06-09 18:37:14 -0700

    Add packaging scripts

---
+++ etc/Util/build_linux_deb.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# Run this script in the parent directory of the repository.
+
+export DJV_BUILD=$PWD
+export PATH=$DJV_BUILD/DJV-install-Release/bin:$PATH
+export LD_LIBRARY_PATH=$DJV_BUILD/DJV-install-Release/lib:$LD_LIBRARY_PATH
+
+mkdir DJV-third-party-Release
+cd DJV-third-party-Release
+cmake ../DJV/third-party -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$DJV_BUILD/DJV-install-Release
+cmake --build . -j 16
+cmake --build . -j 16 --target install
+cd ..
+
+mkdir DJV-Release
+cd DJV-Release
+cmake ../DJV -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$DJV_BUILD/DJV-install-Release -DCMAKE_PREFIX_PATH=$DJV_BUILD/DJV-install-Release
+cmake --build . -j 16
+
+cmake ../DJV -DCPACK_GENERATOR=DEB -DCPACK_PACKAGING_INSTALL_PREFIX=/usr/local/DJV2 -DDJV_THIRD_PARTY=TRUE
+cmake --build . -j 16
+cmake --build . --target package
+
+cmake ../DJV -DCPACK_GENERATOR=TGZ -DCPACK_PACKAGING_INSTALL_PREFIX= -DDJV_THIRD_PARTY=TRUE
+cmake --build . -j 16
+cmake --build . --target package
+
