31023d3a4b4fe27b329d1b678e3e420957efab1c
Darby Johnston <darbyjohnston@yahoo.com>
2020-06-11 14:16:57 -0700

    Rename directories

--- etc/Linux/build_linux_deb.sh
+++ etc/Linux/build_linux_deb.sh
@@ -3,19 +3,19 @@
 # Run this script in the parent directory of the repository.
 
 export DJV_BUILD=$PWD
-export PATH=$DJV_BUILD/DJV-install-Package/bin:$PATH
-export LD_LIBRARY_PATH=$DJV_BUILD/DJV-install-Package/lib:$LD_LIBRARY_PATH
+export PATH=$DJV_BUILD/DJV-install-package/bin:$PATH
+export LD_LIBRARY_PATH=$DJV_BUILD/DJV-install-package/lib:$LD_LIBRARY_PATH
 
-mkdir -p DJV-third-party-Package
-cd DJV-third-party-Package
-cmake ../DJV/third-party -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$DJV_BUILD/DJV-install-Package
+mkdir -p DJV-third-party-package
+cd DJV-third-party-package
+cmake ../DJV/third-party -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$DJV_BUILD/DJV-install-package
 cmake --build . -j 8
 cmake --build . -j 8 --target install
 cd ..
 
-mkdir -p DJV-Package
-cd DJV-Package
-cmake ../DJV -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$DJV_BUILD/DJV-install-Package -DCMAKE_PREFIX_PATH=$DJV_BUILD/DJV-install-Package
+mkdir -p DJV-package
+cd DJV-package
+cmake ../DJV -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$DJV_BUILD/DJV-install-package -DCMAKE_PREFIX_PATH=$DJV_BUILD/DJV-install-package
 cmake --build . -j 8
 
 cmake ../DJV -DCPACK_GENERATOR=DEB -DCPACK_PACKAGING_INSTALL_PREFIX=/usr/local/DJV2 -DDJV_THIRD_PARTY=TRUE
--- etc/OSX/build_macos_dmg.sh
+++ etc/OSX/build_macos_dmg.sh
@@ -3,19 +3,19 @@
 # Run this script in the parent directory of the repository.
 
 export DJV_BUILD=$PWD
-export PATH=$DJV_BUILD/DJV-install-Package/bin:$PATH
-export DYLD_LIBRARY_PATH=$DJV_BUILD/DJV-install-Package/lib:$DYLD_LIBRARY_PATH
+export PATH=$DJV_BUILD/DJV-install-package/bin:$PATH
+export DYLD_LIBRARY_PATH=$DJV_BUILD/DJV-install-package/lib:$DYLD_LIBRARY_PATH
 
-mkdir -p DJV-third-party-Package
-cd DJV-third-party-Package
-cmake ../DJV/third-party -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$DJV_BUILD/DJV-install-Package -DCMAKE_FIND_FRAMEWORK="LAST"
+mkdir -p DJV-third-party-package
+cd DJV-third-party-package
+cmake ../DJV/third-party -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$DJV_BUILD/DJV-install-package -DCMAKE_FIND_FRAMEWORK="LAST"
 cmake --build . -j 8
 cmake --build . -j 8 --target install
 cd ..
 
-mkdir -p DJV-Package
-cd DJV-Package
-cmake ../DJV -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$DJV_BUILD/DJV-install-Package -DCMAKE_PREFIX_PATH=$DJV_BUILD/DJV-install-Package -DCMAKE_FIND_FRAMEWORK="LAST"
+mkdir -p DJV-package
+cd DJV-package
+cmake ../DJV -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$DJV_BUILD/DJV-install-package -DCMAKE_PREFIX_PATH=$DJV_BUILD/DJV-install-package -DCMAKE_FIND_FRAMEWORK="LAST"
 cmake --build . -j 8
 
 cmake ../DJV -DDJV_THIRD_PARTY=TRUE
