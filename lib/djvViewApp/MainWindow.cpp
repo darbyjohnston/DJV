79ff1082bf7ff24fe91b6daeac8201b95e4a2950
Darby Johnston <darbyjohnston@yahoo.com>
2020-08-26 11:33:25 -0700

    Color tweaks

--- lib/djvViewApp/MainWindow.cpp
+++ lib/djvViewApp/MainWindow.cpp
@@ -143,6 +143,7 @@ namespace djv
             auto settingsDrawer = UI::Drawer::create(UI::Side::Right, context);
 
             p.menuBar = UI::MenuBar::create(context);
+            p.menuBar->setBackgroundRole(UI::ColorRole::BackgroundHeader);
             for (auto i : menus)
             {
                 p.menuBar->addChild(i.second);
--- lib/djvViewApp/TimelineWidget.cpp
+++ lib/djvViewApp/TimelineWidget.cpp
@@ -97,6 +97,7 @@ namespace djv
 
             DJV_PRIVATE_PTR();
             setClassName("djv::ViewApp::TimelineWidget");
+            setBackgroundRole(UI::ColorRole::BackgroundToolBar);
 
             p.actions["Forward"] = UI::Action::create();
             p.actions["Forward"]->setIcon("djvIconPlaybackForward");
