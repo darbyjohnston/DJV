9a8eddf3ac20dc0ecebc1200dc2a7b36c4691520
Darby Johnston <darbyjohnston@yahoo.com>
2025-06-25 17:22:07 -0700

    Icon updates

--- etc/macOS/icon.sh
+++ etc/macOS/icon.sh
@@ -1,1 +1,8 @@
+cp ../Icons/DJV_Icon_512.png DJV.iconset/icon_512x512.png
+cp ../Icons/DJV_Icon_256.png DJV.iconset/icon_256x256.png
+cp ../Icons/DJV_Icon_128.png DJV.iconset/icon_128x128.png
+cp ../Icons/DJV_Icon_64.png DJV.iconset/icon_64x64.png
+cp ../Icons/DJV_Icon_32.png DJV.iconset/icon_32x32.png
+cp ../Icons/DJV_Icon_16.png DJV.iconset/icon_16x16.png
+
 iconutil -c icns DJV.iconset
