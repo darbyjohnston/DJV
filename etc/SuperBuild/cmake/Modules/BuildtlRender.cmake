ee01d7ca2e4c35d909a98d71123ba44ea394b82e
Darby Johnston <darbyjohnston@yahoo.com>
2025-07-14 10:26:35 -0700

    Update tlRender version

--- etc/SuperBuild/cmake/Modules/BuildtlRender.cmake
+++ etc/SuperBuild/cmake/Modules/BuildtlRender.cmake
@@ -1,7 +1,7 @@
 include(ExternalProject)
 
 set(TLRENDER_GIT_REPOSITORY "https://github.com/darbyjohnston/tlRender.git")
-set(TLRENDER_GIT_TAG "514fa60a5d18d3f8b54a0963edf5a1b03e674deb")
+set(TLRENDER_GIT_TAG "9841accf1a190d5ec8e5939ef277765bfa5b9f9a")
 
 set(TLRENDER_DEPS)
 set(TLRENDER_ARGS
