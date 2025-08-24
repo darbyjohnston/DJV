2ce4d790c0839362d28301fc6b1c7077c2e4d609
Darby Johnston <darbyjohnston@yahoo.com>
2025-08-24 13:30:10 -0700

    Add BMD options

--- SuperBuildLinux.sh
+++ SuperBuildLinux.sh
@@ -12,6 +12,8 @@ export TLRENDER_OCIO=ON
 export TLRENDER_FFMPEG=ON
 export TLRENDER_FFMPEG_MINIMAL=OFF
 export TLRENDER_USD=OFF
+export TLRENDER_BMD=OFF
+export TLRENDER_BMD_SDK=
 export FEATHER_TK_API=GL_4_1
 
 sh DJV/etc/Linux/linux-build-gha.sh $BUILD_TYPE
